691f54412ab35e7b598953ed20adc83f1398856d
sammeyer111 <samuel.aaron.meyer@gmail.com>
2025-10-10 10:40:53 -0400

    Created test for new tailwind site

--- tests/test_homepage_h1.js
+++ tests/test_homepage_h1.js
@@ -4,7 +4,13 @@ test("page loads and has an H1", async ({ page }, testInfo) => {
   await page.goto("/");
 
   const h1 = page.locator("h1");
+  const header = page.locator("header");
+  const nav = page.locator("nav");
+  const button = page.locator("button");
   await expect(h1).toBeVisible();
+  await expect(header).toBeVisible();
+  await expect(nav).toBeVisible();
+  await expect(button).toBeVisible();
 
   await page.screenshot({
     path: testInfo.outputPath("homepage_h1.png"),
