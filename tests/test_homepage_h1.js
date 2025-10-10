425404a31fbda6da51ccff10a3e604f0cef4e5c1
Aaron Espenshade <awespenshade@gmail.com>
2025-10-10 10:44:37 -0400

    Friday 10/10

--- tests/test_homepage_h1.js
+++ tests/test_homepage_h1.js
@@ -6,6 +6,14 @@ test("page loads and has an H1", async ({ page }, testInfo) => {
   const h1 = page.locator("h1");
   await expect(h1).toBeVisible();
 
+  const header = page.locator("header");
+  const nav = page.locator("nav");
+  const main = page.locator("main");
+
+  await expect(header).toBeVisible();
+  await expect(nav).toBeVisible();
+  await expect(main).toBeVisible();
+
   //Single screenshot
   await page.screenshot({ path: testInfo.outputPath("homepage_h1.png"), fullPage: true });
 });
